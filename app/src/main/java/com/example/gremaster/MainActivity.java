303a029564ee5de5646b82be36ac5e241a97db40
Nafis Fuad Abir <nfabir7@gmail.com>
2021-12-20 22:59:56 +0600

    data retrieve

--- app/src/main/java/com/example/gremaster/MainActivity.java
+++ app/src/main/java/com/example/gremaster/MainActivity.java
@@ -1,45 +1,134 @@
 package com.example.gremaster;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
-import android.view.Menu;
+import android.text.Layout;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.messaging.FirebaseMessaging;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Objects;
 
-    private FirebaseAuth mAuth;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    String userId, email;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    TextView navName, navStatus;
+    ImageView navImage;
+
+    FirebaseAuth mAuth;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //FirebaseMessaging.getInstance().subscribeToTopic("Vocabulary");
+
         mAuth = FirebaseAuth.getInstance();
+
+        loadData();
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navView);
+        toolbar = findViewById(R.id.toolbar);
+        View navHeader = getLayoutInflater().inflate(R.layout.nav_header,navigationView);
+        navName = (TextView) navHeader.findViewById(R.id.textView9);
+        navStatus = (TextView) navHeader.findViewById(R.id.textView10);
+        navImage = navHeader.findViewById(R.id.navImage);
+
+        navImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //getSupportActionBar().hide();
+
+        setSupportActionBar(toolbar);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
 
-        getMenuInflater().inflate(R.menu.menu_logout,menu);
 
-        return super.onCreateOptionsMenu(menu);
-    }
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        if(item.getItemId()==R.id.logout){
-            FirebaseAuth.getInstance().signOut();
-            finish();
+        if(currentUser==null)
+        {
             Intent intent = new Intent(MainActivity.this,LoginActivity.class);
             startActivity(intent);
+            finish();
         }
+    }
 
+    public void loadData(){
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user!=null) userId = user.getUid();
+
+        reference = FirebaseDatabase.getInstance().getReference("users");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()) {
+                    String name = snapshot.child(userId).child("name").getValue(String.class);
+                    String expert = snapshot.child(userId).child("expert").getValue(String.class);
+
+                    navName.setText(name);
+                    navStatus.setText("GRE Expert: "+expert);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.logout){
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+            startActivity(intent);
+            finish();
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 }
